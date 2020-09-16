0b288811cd870169e6efb46cf40d3303e03c2bed
Pursuit <pursuit@Pursuits-Air.lan>
2020-09-16 01:47:48 -0400

    made more skeletons for page

--- src/components/blueprint.js
+++ src/components/blueprint.js
@@ -5,55 +5,89 @@ export default class Blueprint extends Component {
   render() {
     return (
       <>
-        <div style={{ marginBottom: "1rem", marginTop: "1rem" }}>
+        <div style={{ margin: "1rem" }}>
           <Skeleton variant="text" width={105} />
           <Skeleton variant="text" width={210} />
           <Skeleton variant="rect" width={210} height={118} />
         </div>
 
-        <div style={{ marginBottom: "1rem", marginTop: "1rem" }}>
+        <div style={{ margin: "1rem" }}>
           <Skeleton variant="text" width={105} />
           <Skeleton variant="text" width={210} />
           <Skeleton variant="rect" width={210} height={118} />
         </div>
 
-        <div style={{ marginBottom: "1rem", marginTop: "1rem" }}>
+        <div style={{ margin: "1rem" }}>
           <Skeleton variant="text" width={105} />
           <Skeleton variant="text" width={210} />
           <Skeleton variant="rect" width={210} height={118} />
         </div>
 
-        <div style={{ marginBottom: "1rem", marginTop: "1rem" }}>
+        <div style={{ margin: "1rem" }}>
           <Skeleton variant="text" width={105} />
           <Skeleton variant="text" width={210} />
           <Skeleton variant="rect" width={210} height={118} />
         </div>
 
-        <div style={{ marginBottom: "1rem", marginTop: "1rem" }}>
+        <div style={{ margin: "1rem" }}>
           <Skeleton variant="text" width={105} />
           <Skeleton variant="text" width={210} />
           <Skeleton variant="rect" width={210} height={118} />
         </div>
 
-        <div style={{ marginBottom: "1rem", marginTop: "1rem" }}>
+        <div style={{ margin: "1rem" }}>
           <Skeleton variant="text" width={105} />
           <Skeleton variant="text" width={210} />
           <Skeleton variant="rect" width={210} height={118} />
         </div>
 
-        <div style={{ marginBottom: "1rem", marginTop: "1rem" }}>
+        <div style={{ margin: "1rem" }}>
           <Skeleton variant="text" width={105} />
           <Skeleton variant="text" width={210} />
           <Skeleton variant="rect" width={210} height={118} />
         </div>
 
-        <div style={{ marginBottom: "1rem", marginTop: "1rem" }}>
+        <div style={{ margin: "1rem" }}>
           <Skeleton variant="text" width={105} />
           <Skeleton variant="text" width={210} />
           <Skeleton variant="rect" width={210} height={118} />
         </div>
 
-        <div style={{ marginBottom: "1rem", marginTop: "1rem" }}>
+        <div style={{ margin: "1rem" }}>
+          <Skeleton variant="text" width={105} />
+          <Skeleton variant="text" width={210} />
+          <Skeleton variant="rect" width={210} height={118} />
+        </div>
+        <div style={{ margin: "1rem" }}>
+          <Skeleton variant="text" width={105} />
+          <Skeleton variant="text" width={210} />
+          <Skeleton variant="rect" width={210} height={118} />
+        </div>
+
+        <div style={{ margin: "1rem" }}>
+          <Skeleton variant="text" width={105} />
+          <Skeleton variant="text" width={210} />
+          <Skeleton variant="rect" width={210} height={118} />
+        </div>
+
+        <div style={{ margin: "1rem" }}>
+          <Skeleton variant="text" width={105} />
+          <Skeleton variant="text" width={210} />
+          <Skeleton variant="rect" width={210} height={118} />
+        </div>
+
+        <div style={{ margin: "1rem" }}>
+          <Skeleton variant="text" width={105} />
+          <Skeleton variant="text" width={210} />
+          <Skeleton variant="rect" width={210} height={118} />
+        </div>
+
+        <div style={{ margin: "1rem" }}>
+          <Skeleton variant="text" width={105} />
+          <Skeleton variant="text" width={210} />
+          <Skeleton variant="rect" width={210} height={118} />
+        </div>
+        <div style={{ margin: "1rem" }}>
           <Skeleton variant="text" width={105} />
           <Skeleton variant="text" width={210} />
           <Skeleton variant="rect" width={210} height={118} />
